93252d5d8156717603503a36acbd856fddad0522
unknown <niv.caspi@TLV-NOC2-DT.e4x.com>
2016-01-26 18:40:14 +0200

    sol6

--- set1/hex_to_base64.py
+++ set1/hex_to_base64.py
@@ -95,10 +95,8 @@ with open('6.txt', 'rb') as f2:
       f1.write(str(ord(c)) + '\t')
 
 
-'''
 hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 print hex_to_base64(hex_str)
 b64_str ='SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-bytearray(b64_str)
+b64_str = bytearray(b64_str)
 print base64_to_string(b64_str)
-'''
